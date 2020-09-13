7725e318b738952f3638f5331ef825389bb9e625
divya12prakash <divya12prakash@gmail.com>
2020-09-13 11:33:16 -0400

    Factorial and DFS with Adjacency

---
+++ src/main/java/com/dp/basicalgorithms/DFSAdjacencyList.java
@@ -0,0 +1,40 @@
+package com.dp.basicalgorithms;
+
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class DFSAdjacencyList {
+
+    private int vertices;
+
+    private LinkedList[] adjacency;
+
+
+    DFSAdjacencyList(int size){
+        adjacency = new LinkedList[size];
+        for(int i =0; i<size; i++){
+            adjacency[i] = new LinkedList();
+        }
+    }
+
+
+    private void DFSUtil(int index, boolean[] visited){
+        visited[index] = true;
+
+        Iterator<Integer> list = adjacency[index].listIterator();
+
+        while(list.hasNext()){
+            int n = list.next();
+            if(!visited[n]){
+                DFSUtil(n,visited);
+            }
+        }
+    }
+
+
+    private void DFS(int index){
+        boolean[] visited = new boolean[vertices];
+
+        DFSUtil(index,visited);
+    }
+}
