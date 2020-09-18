f24c414326a3e70c6d8ba3db5e2a7a5a2f0436b6
divya12prakash <divya12prakash@gmail.com>
2020-09-18 10:36:30 -0400

    Topo Sort Implementation with Stack

---
+++ src/main/java/com/dp/basicalgorithms/BFSQueue.java
@@ -0,0 +1,53 @@
+package com.dp.basicalgorithms;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Implement Breadth first search using a queue data structure.
+ */
+public class BFSQueue {
+
+    Queue<BFSNode> queue;
+
+    void search(BFSNode initialNode) {
+
+        queue.add(initialNode);
+        initialNode.visited = Boolean.TRUE;
+
+        while (!queue.isEmpty()){
+            BFSNode bfsNode = queue.remove();
+
+            List<BFSNode> neigh = (List<BFSNode>) bfsNode.neighbours.listIterator();
+
+            for(BFSNode bfsNode1: neigh){
+                if(!bfsNode1.visited && bfsNode1!=null) {
+                    bfsNode1.visited = true;
+                    queue.add(bfsNode1);
+                }
+            }
+        }
+
+    }
+
+
+}
+
+@Getter
+@Setter
+class BFSNode {
+    int value;
+    List<Integer> neighbours;
+    boolean visited;
+
+    BFSNode(int value) {
+        this.value = value;
+        this.neighbours = new ArrayList<>();
+    }
+}
---
+++ src/main/java/com/dp/basicalgorithms/TopologicalSort.java
@@ -0,0 +1,55 @@
+package com.dp.basicalgorithms;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Stack;
+
+/**
+ * Topological sort with Adjacency list
+ */
+public class TopologicalSort {
+
+    private int vertex;
+    private ArrayList<ArrayList<Integer>> adj;
+
+    public TopologicalSort(int size){
+        this.vertex = size;
+        this.adj = new ArrayList<ArrayList<Integer>>(size);
+
+        for(int i=0; i<size; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        }
+    public void addEdge(int v, int e){
+        adj.get(v).add(e);
+    }
+
+
+    void topoSort(){
+        Stack<Integer> stack = new Stack();
+        boolean[] visited = new boolean[vertex];
+
+        for(int i=0; i<vertex; i++){
+            if(visited[i]==false) {
+                topoSortUtil(i, visited, stack);
+            }
+        }
+    }
+
+    private void topoSortUtil(int i, boolean[] visited, Stack<Integer> stack) {
+        visited[i] = true;
+
+        Iterator itr = this.adj.get(i).iterator();
+        while(itr.hasNext()){
+        int value = (Integer) itr.next();
+
+        if(!visited[value]){
+            topoSortUtil(value,visited,stack);
+        }
+        }
+        stack.push(i);
+    }
+}
+
