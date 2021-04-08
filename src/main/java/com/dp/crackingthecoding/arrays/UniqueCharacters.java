fd91496c226b72e7e70f8641c7f5055a64663410
divya12prakash <divya12prakash@gmail.com>
2021-04-08 17:48:12 -0400

    Other methods to find unique chars in a string

--- src/main/java/com/dp/crackingthecoding/arrays/UniqueCharacters.java
+++ src/main/java/com/dp/crackingthecoding/arrays/UniqueCharacters.java
@@ -1,30 +1,70 @@
 package com.dp.crackingthecoding.arrays;
 
+import java.util.Arrays;
+
 /**
- * Exercise 1.1 find unique characters in a string
+ * Exercise 1.1 find if a string has all unique characters
  */
 public class UniqueCharacters {
 
 
     /**
      * do not use O(N2)
+     *
      * @param input
      * @return
      */
-    public boolean bruteForce(String input){
+    public boolean bruteForce(String input) {
         char[] chars = new char[input.length()];
 
-        for(int i=0; i<input.length(); i++){
+        for (int i = 0; i < input.length(); i++) {
             chars[i] = input.charAt(i);
         }
-        for(int i=0; i< chars.length; i++){
-            for(int j=i+1; j < chars.length; j++){
-                if(chars[i] == chars[j]) return false;
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = i + 1; j < chars.length; j++) {
+                if (chars[i] == chars[j]) return false;
             }
         }
         return true;
     }
 
+    /**
+     * nlogn complexity
+     * @param input
+     * @return
+     */
+    public boolean sortAndCompare(String input) {
+        char[] chars = input.toCharArray();
+        Arrays.sort(chars);
+        for (int i = 0; i < chars.length - 1; i++) {
+            if (chars[i] == chars[i + 1]) return false;
+        }
+        return true;
+    }
+
+    /**
+     * O(N)
+     * @param input
+     * @return
+     */
+    public boolean declareArray(String input){
+
+        int max = 256;
+
+        boolean[] boo = new boolean[max];
+        Arrays.fill(boo, false);
+
+        for(int i=0; i < input.toCharArray().length ; i++){
+            int index = input.charAt(i);
+            if(!boo[index]) {
+                boo[index] = true;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 
 }
--- src/test/java/com/dp/datastructures/cracking/UniqueStringTest.java
+++ src/test/java/com/dp/datastructures/cracking/UniqueStringTest.java
@@ -5,11 +5,24 @@ import org.junit.jupiter.api.Test;
 
 public class UniqueStringTest {
 
-    UniqueCharacters uniqueCharacters = new UniqueCharacters();
+    private UniqueCharacters uniqueCharacters = new UniqueCharacters();
 
     @Test
     public void testBruteForce(){
         String input = "dasha";
         System.out.println(uniqueCharacters.bruteForce(input));
+    }
+
+
+    @Test
+    public void testsorted(){
+        String input = "dash";
+        System.out.println(uniqueCharacters.sortAndCompare(input));
+    }
+
+    @Test
+    public void testExtraSpace(){
+        String input = "dasha";
+        System.out.println(uniqueCharacters.declareArray(input));
     }
 }
