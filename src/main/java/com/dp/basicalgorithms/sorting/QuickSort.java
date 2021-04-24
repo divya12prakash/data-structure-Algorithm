a6b701dc18e1ba5b1fd30743f9079969427efe4a
divya12prakash <divya12prakash@gmail.com>
2021-04-24 18:13:50 -0400

    Quick sort

--- src/main/java/com/dp/basicalgorithms/sorting/QuickSort.java
+++ src/main/java/com/dp/basicalgorithms/sorting/QuickSort.java
@@ -1,4 +1,39 @@
 package com.dp.basicalgorithms.sorting;
 
 public class QuickSort {
+
+
+    public void quickSor(int[] array, int start, int end) {
+
+        if (start < end) {
+
+            int p = sort(array, start, end);
+
+            quickSor(array, start, p - 1);
+            quickSor(array, p + 1, end);
+
+        }
+
+    }
+
+    private int sort(int[] array, int start, int end) {
+
+        int pivot = array[end];
+        int j = start;
+
+        for (int i = start; i < end; i++) {
+            if (array[i] < pivot) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                j++;
+            }
+        }
+
+        int tmp = array[j];
+        array[j] = array[end];
+        array[end] = tmp;
+
+        return j;
+    }
 }
