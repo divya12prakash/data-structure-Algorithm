bde0e8b9336e066e74877ed44b4773e746e26fb6
divya12prakash <divya12prakash@gmail.com>
2021-06-20 13:42:05 -0400

    find duplicates in an array using Floyds Algorithm of Linked lists

--- src/main/java/com/dp/q450/arrays/FindDuplicatesInArray.java
+++ src/main/java/com/dp/q450/arrays/FindDuplicatesInArray.java
@@ -18,4 +18,35 @@ public class FindDuplicatesInArray {
         return output;
     }
 
+
+    /**
+     * Using Floyd's algorithm. You start with fast and slow pointers
+     * Detect the first position where the two intersect. Get rid of the fast
+     * pointer there.
+     * Next step is to start with another slow pointer from the beginning
+     * Move both slow pointers and find the position where they will
+     * intersect next.
+     */
+
+
+    public int floydsAlgo(int[] input) {
+        int fast = input[input[0]];
+        int slow = input[0];
+
+
+        while(slow != fast) {
+            slow = input[slow];
+            fast = input[input[fast]];
+        }
+
+        fast = 0;
+
+        while(slow != fast) {
+            slow = input[slow];
+            fast = input[fast];
+        }
+
+        return slow;
+    }
+
 }
--- src/test/java/com/dp/datastructures/q450/FindDuplicatesTest.java
+++ src/test/java/com/dp/datastructures/q450/FindDuplicatesTest.java
@@ -5,12 +5,23 @@ import org.junit.jupiter.api.Test;
 
 public class FindDuplicatesTest {
 
-    int[] input =  {1, 2, 3, 6, 3, 6, 1};
+    int[] input = {1, 2, 3, 6, 3, 6, 1};
+
+    int[] inputFld = {1,2,3,3};
+
+    FindDuplicatesInArray f = new FindDuplicatesInArray();
+
 
     @Test
-            public void test() {
-        FindDuplicatesInArray f = new FindDuplicatesInArray();
-
+    public void test() {
         System.out.println(f.findDups(input));
+    }
+
+
+    @Test
+    public void testFloys() {
+
+        System.out.println(f.floydsAlgo(inputFld));
+
     }
 }
