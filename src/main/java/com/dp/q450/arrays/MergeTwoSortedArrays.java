a3cec8966afd98bc107d9bc1b80210262e668ae5
divya12prakash <divya12prakash@gmail.com>
2021-06-21 13:44:37 -0400

    Merge two sorted arrays in the first one

--- src/main/java/com/dp/q450/arrays/MergeTwoSortedArrays.java
+++ src/main/java/com/dp/q450/arrays/MergeTwoSortedArrays.java
@@ -15,14 +15,36 @@ public class MergeTwoSortedArrays {
             output[k++] = arr1[i] < arr2[j] ? arr1[i++] : arr2[j++];
         }
 
-        while(i < firstPointer){
+        while (i < firstPointer) {
             output[k++] = arr1[i++];
         }
 
-        while ( j < secondPointer) {
+        while (j < secondPointer) {
             output[k++] = arr2[j++];
         }
 
         return output;
+    }
+
+    /**
+     * The assumption is the first array is of enough size to
+     * accommodate the elements of the second array
+     * First array is the larger array
+     *
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public int[] mergeInTheSameArray(int[] arr1, int m, int[] arr2, int n) {
+        int p1 = m - 1, p2 = n - 1, i = m + n - 1;
+
+        while (p2 >= 0) {
+            if (p1 >= 0 && arr1[p1] > arr2[p2]) {
+                arr1[i--] = arr1[p1--];
+            } else arr1[i--] = arr2[p2--];
+        }
+
+        return arr1;
+
     }
 }
