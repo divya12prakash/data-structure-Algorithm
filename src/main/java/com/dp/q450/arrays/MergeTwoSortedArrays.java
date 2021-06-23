c506085b8a238a84a381b0e63104635e610ba374
divya12prakash <divya12prakash@gmail.com>
2021-06-22 21:01:03 -0400

    Merge two sorted arrays with O(n2)

--- src/main/java/com/dp/q450/arrays/MergeTwoSortedArrays.java
+++ src/main/java/com/dp/q450/arrays/MergeTwoSortedArrays.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class MergeTwoSortedArrays {
 
+    static int arr1[] = new int[]{1, 5, 9, 10, 15, 20};
+    static int arr2[] = new int[]{2, 3, 8, 13};
 
     public int[] merge(int[] arr1, int[] arr2) {
 
@@ -50,17 +52,12 @@ public class MergeTwoSortedArrays {
 
     }
 
-
-    static int arr1[] = new int[] { 1, 5, 9, 10, 15, 20 };
-    static int arr2[] = new int[] { 2, 3, 8, 13 };
-
     // Function to merge two arrays
-    static void merge(int m, int n)
-    {
+    static void merge(int m, int n) {
 
-        int i= m-1, j=0;
-        while(i>=0 && j<n){
-            if(arr1[i] > arr2[j]){
+        int i = m - 1, j = 0;
+        while (i >= 0 && j < n) {
+            if (arr1[i] > arr2[j]) {
                 arr1[i] = arr1[i] ^ arr2[j] ^ (arr2[j++] = arr1[i--]);
             } else break;
         }
@@ -68,9 +65,32 @@ public class MergeTwoSortedArrays {
         Arrays.sort(arr2);
     }
 
-    public static void main(String[] args)
-    {
-        merge(arr1.length, arr2.length);
+    /**
+     * compare first element of second array with the elements
+     * of first array. replace if the element in first array is
+     * greater. Sort the second array after swap and compare again
+     * this is O(m*n)
+     *
+     * @param arr1
+     * @param arr2
+     */
+    static void mergeMN(int[] arr1, int[] arr2) {
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] > arr2[0]) {
+                arr1[i] = arr1[i] ^ arr2[0] ^ (arr2[0] = arr1[i]);
+
+                for (int j = 0; j < arr2.length - 1 && arr2[j] > arr2[j + 1]; j++) {
+                    arr2[j] = arr2[j] ^ arr2[j + 1] ^ (arr2[j + 1] = arr2[j]);
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        mergeMN(arr1, arr2);
         System.out.print("After Merging \nFirst Array: ");
         System.out.println(Arrays.toString(arr1));
         System.out.print("Second Array:  ");
