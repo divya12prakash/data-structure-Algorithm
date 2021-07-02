2c7d07361d5128f3f2248cdf8379c4d8198739ab
divya12prakash <divya12prakash@gmail.com>
2021-07-01 21:30:37 -0400

    Buy and Sell stock any number of times

--- src/main/java/com/dp/q450/arrays/BuySelI2.java
+++ src/main/java/com/dp/q450/arrays/BuySelI2.java
@@ -21,4 +21,35 @@ public class BuySelI2 {
 
         return maxProfit;
     }
+
+    /**
+     * DP approach. The logic is to store the max value from right side
+     * and min value difference from left side. ultimately add the two
+     * values in the new array to find the final maximum profit
+     * The max profit will always be arr[n-1]
+     */
+
+    public int getProfitDP(int arr[]) {
+        int[] dp = new int[arr.length];
+
+        int n = arr.length;
+
+        int max=arr[arr.length - 1], min = arr[0];
+
+        for(int index = 0; index < arr.length; index++) dp[index] = 0;
+
+        //Iterate from right to left to find maximum value from right side
+
+        for(int i= n-1; i >=0; i--) {
+            if(arr[i] > max) max = arr[i];
+            dp[i] = Math.max(dp[i+1], max - arr[i]);
+        }
+
+        for(int i=1; i < n; i++) {
+            if(min < arr[i]) min = arr[i];
+            dp[i] = Math.max(dp[i-1], dp[i] + (arr[i] - min));
+        }
+
+        return dp[n-1];
+    }
 }
